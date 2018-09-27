2b732f7fe23f19c26719900823ecfb6d3e980a8e
Peter Barbatsis <pbarbats@openup0-42.bravo>
2018-09-26 22:45:29 -0500

    added data fields to data class Photo for JSON parsing

--- app/src/main/java/nerderylabs/com/jbarbatsflickr/Photo.kt
+++ app/src/main/java/nerderylabs/com/jbarbatsflickr/Photo.kt
@@ -3,9 +3,13 @@ package nerderylabs.com.jbarbatsflickr
 
 // Populate this with fields from the GSON/JSON whatever
 data class Photo(
-        val url: String,
-        val width: Int,
-        val height: Int,
-        val isPrimary: Boolean,
-        val isVideo: Boolean
+        val id: Int,
+        val owner: String,
+        val secret: String,
+        val server: Int,
+        val farm: Int,
+        val title: String,
+        val isPublic: Boolean,
+        val isFriend: Boolean,
+        val isFamily: Boolean
 )
